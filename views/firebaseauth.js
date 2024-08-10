bd4bfd44502b3341ab6cd20286ff12755ee3eeb6
Sunil Hegde <sunil1hegde@gmail.com>
2024-08-10 12:01:09 +0530

    Changed location of index.html

--- views/firebaseauth.js
+++ views/firebaseauth.js
@@ -1,14 +1,14 @@
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
  import {getAuth,createUserWithEmailAndPassword,signInWithEmailAndPassword,sendPasswordResetEmail} from  "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js"
  import{getFirestore, setDoc,doc} from  "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js"
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyBsbAkrZy_mLQUZdLOFF64ASnGHQiAQ8Ag",
     authDomain: "codefury-eb612.firebaseapp.com",
     projectId: "codefury-eb612",
     storageBucket: "codefury-eb612.appspot.com",
     messagingSenderId: "186913768603",
     appId: "1:186913768603:web:f9a79f83d46e03d32c323e"
-  };
+};
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
