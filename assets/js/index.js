4ac5e7c5f5b7509a1ee792b4fb1181f4f8f068e1
Arya-Pai <aryabhaskarpai@gmail.com>
2024-08-10 11:57:08 +0530

    Notification carasouel added

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,13 @@ function hideSidebar(){
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.display = "none";
 }
+
+const carousel = document.getElementById('carousel');
+
+
+setTimeout(() => {
+    const newNotification = document.createElement('div');
+    newNotification.className = 'notification';
+    newNotification.textContent = 'Notification 4: A new event is coming up!';
+    carousel.appendChild(newNotification);
+}, 5000);
